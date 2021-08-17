d63bb42a218351df2c7b9c533508bf8fba63e975
Buğra <584254-bugrargn@users.noreply.gitlab.com>
2021-08-17 15:40:47 +0300

    Quiz List

---
+++ resources/views/admin/quiz/list.blade.php
@@ -0,0 +1,34 @@
+<x-app-layout>
+    <x-slot name="header">
+        Quizler
+    </x-slot>
+    <div class="card-body">
+        <h5 class="card-title">
+            <a href="" class="btn btn-sm btn-primary">Quiz Oluştur</a>
+        </h5>
+        <table class="table table-bordered bg-white">
+            <thead>
+                <tr>
+                    <th scope="col">Quiz</th>
+                    <th scope="col">Durum</th>
+                    <th scope="col">Bitiş Tarihi</th>
+                    <th scope="col">İşlemler</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($quizzes as $quiz)                    
+                <tr>
+                    <td>{{$quiz->title}}</td>
+                    <td>{{$quiz->status}}</td>
+                    <td>{{$quiz->finished_at}}</td>
+                    <td>
+                        <a href="" class="btn-sm btn-primary">Düzenle</a>
+                        <a href="" class="btn-sm btn-danger">Sil</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{$quizzes->links()}}
+    </div>
+</x-app-layout>
